251e3b948b386e7f915bf16ef5b112f7b2220aa6
akhil358-star <85740508+akhil358-star@users.noreply.github.com>
2022-10-20 02:12:58 -0700

    updated java file as 7th commit
    added required attributes and uploaded java file

---
+++ ClassProductList.java
@@ -0,0 +1,13 @@
+public class ClassProductList {
+
+	private ProductIterator productIterator;
+
+	private Product[] product;
+
+	private ReminderVisitor reminderVisitor;
+
+	public void accept(visitor : nodeVisitor)() {
+
+	}
+
+}
