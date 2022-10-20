6dca0ff020d12fa80df31926f59e84de18f5ab57
akhil358-star <85740508+akhil358-star@users.noreply.github.com>
2022-10-20 02:16:30 -0700

    updated java file as 10th commit
    uploaded java file

---
+++ ProductIterator.java
@@ -0,0 +1,21 @@
+public class ProductIterator {
+
+	private ClassProductList classProductList;
+
+	public boolean hasNext() {
+
+	}
+
+	public Offering Next() {
+
+	}
+
+	public void MoveToHead() {
+
+	}
+
+	public void Remove() {
+
+	}
+
+}
