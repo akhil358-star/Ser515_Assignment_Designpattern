cc2e899b9b61c0423590182982cc97a82845a582
akhil358-star <85740508+akhil358-star@users.noreply.github.com>
2022-10-20 02:15:39 -0700

    updated java file as 9th commit
    uploaded java file

---
+++ OfferingIterator.java
@@ -0,0 +1,21 @@
+public class OfferingIterator {
+
+	private OfferingList offeringList;
+
+	public boolean hasNext() {
+
+	}
+
+	public Offering Next() {
+
+	}
+
+	public void MoveToHead() {
+
+	}
+
+	public void Remove() {
+
+	}
+
+}
